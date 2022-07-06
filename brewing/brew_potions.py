5dcae9d54e84ac8114cc2ad2b76793fb8630d2ba
ivanzea <ivnzea@gmail.com>
2022-07-06 12:46:15 +0200

    expert potion function works

--- brewing/brew_potions.py
+++ brewing/brew_potions.py
@@ -19,13 +19,18 @@ def make_example_potion(name):
 def make_python_expert_potion(name):
     print("I am a Python Expert")
     # todo: write this function!
-    return
+    my_potion = potion_class.Potion(name=name)
+    my_potion.setup(container=containers.pewter_cauldron, heat_source=cooking.fire)
+    my_potion.add_ingredients(["fish_eyes","unicorn_hair","tea_leaves"])
+    cooking.simmer(my_potion, 2)
+
+    return my_potion
 
 
 if __name__ == "__main__":
     my_name = 'GSN student'
-    my_potion = make_example_potion(my_name)
+    my_potion = make_python_expert_potion(my_name)
     # Let Snape inspect the potion
-    inspection.inspection_by_Snape(potion=my_potion, target_potion='example_potion')
+    inspection.inspection_by_Snape(potion=my_potion, target_potion='python_expert')
 
 
