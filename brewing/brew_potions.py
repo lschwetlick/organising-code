ea52c22fdb6002d13d1855dfe64a8bb232563d36
Moritz Gerster <moritz.gerster@posteo.de>
2022-07-06 12:47:29 +0200

    Make python expert function

--- brewing/brew_potions.py
+++ brewing/brew_potions.py
@@ -1,7 +1,7 @@
-import potion_class
-import containers
-import cooking
-import inspection
+from brewing import potion_class
+from brewing import containers
+from brewing import cooking
+from brewing import inspection
 
 
 def make_example_potion(name):
@@ -18,14 +18,21 @@ def make_example_potion(name):
 
 def make_python_expert_potion(name):
     print("I am a Python Expert")
-    # todo: write this function!
-    return
+    my_potion = potion_class.Potion(name=name)
+    my_potion.add_ingredients(['fish_eyes', 'tea_leaves', 'unicorn_hair'])
+    # Set up your old kettle and light an eternal flame underneath it.
+    my_potion.setup(container=containers.pewter_cauldron, heat_source=cooking.fire)
+    # Simmer for 5 hours.
+    cooking.simmer(my_potion, duration=2)
+
+    return my_potion
 
 
 if __name__ == "__main__":
     my_name = 'GSN student'
-    my_potion = make_example_potion(my_name)
+    # my_potion = make_example_potion(my_name)
+    my_potion = make_python_expert_potion(my_name)
     # Let Snape inspect the potion
-    inspection.inspection_by_Snape(potion=my_potion, target_potion='example_potion')
+    inspection.inspection_by_Snape(potion=my_potion, target_potion='python_expert')
 
 
